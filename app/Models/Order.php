5967610d9a57141fff29374360cb3834114665cf
Rodglenn Rojas <bushidosnap@gmail.com>
2021-04-19 15:52:40 +0800

    second commit

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,7 +10,7 @@ class Order extends Model
     
     use HasFactory;
 
-    // protected $fillable = ['user_id','tailor_id','date_finish','total bill','status'];
+    protected $fillable = ['user_id','tailor_id','date_finish','total bill','status'];
 
 
     public function user(){
